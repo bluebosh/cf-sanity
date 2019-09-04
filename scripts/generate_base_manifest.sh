f7a7315c210e09c9e511b40750931093a0cd643d
Zida <shawzida@gmail.com>
2019-09-04 17:36:32 +0800

    Invokde bazel build and apply ops without eirini

--- scripts/generate_base_manifest.sh
+++ scripts/generate_base_manifest.sh
@@ -14,49 +14,64 @@
 set -eEu -o pipefail
 shopt -s extdebug
 
+GIT_ROOT=${GIT_ROOT:-$(git rev-parse --show-toplevel)}
+
+ENABLE_EIRINI=false
+
+# get pre_start_script ops files from bazel configuration
+pushd "${GIT_ROOT}/scf"
+  bazel build //deploy/helm/scf:chart
+popd
+
 # Download cf_deployment
 cf_deployment_version=8.0.0
 cf_deployment_yaml_url=https://raw.githubusercontent.com/cloudfoundry/cf-deployment/v${cf_deployment_version}/cf-deployment.yml
 #shasum="1c464acccdd679400005775340ccb69ef8c41d69"
 
-curl -L -o cf-deployment.yml $cf_deployment_yaml_url
+curl -L -o "${GIT_ROOT}/cf-deployment.yml" $cf_deployment_yaml_url
 #echo "${shasum} cf-deployment.yml" | sha1sum -c -
 
 # Apply ops files
-bosh interpolate cf-deployment.yml \
- -o scf/deploy/helm/scf/assets/operations/temporary/remove_roles.yaml \
- -o scf/deploy/helm/scf/assets/operations/temporary/remove_variables.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/adapter.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/api.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/cc-worker.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/database.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/diego-api.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/diego-cell.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/doppler.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/log-api.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/nats.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/router.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/scheduler.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/singleton-blobstore.yaml \
- -o scf/deploy/helm/scf/assets/operations/instance_groups/uaa.yaml \
- -o scf/deploy/helm/scf/assets/operations/addons.yaml \
- -o scf/deploy/helm/scf/assets/operations/certs.yaml \
- -o scf/deploy/helm/scf/assets/operations/set_opensuse_stemcells.yaml \
- -o scf/deploy/helm/scf/assets/operations/set_release_urls.yaml \
- -o scf/deploy/helm/scf/assets/operations/set_release_versions.yaml \
- -v releases-defaults-url=docker.io/cfcontainerization
+interpolate_cmd="bosh interpolate ${GIT_ROOT}/cf-deployment.yml  "
 
-# TODO How to extract and apply pre_start.sh from bazel configuration
+search_temporary_ops_dir="${GIT_ROOT}/scf/deploy/helm/scf/assets/operations/temporary/*.yaml"
+for file in $search_temporary_ops_dir
+do
+  interpolate_cmd="${interpolate_cmd}  -o $file  "
+done
 
-script_files="scf/bosh/releases/pre_render_scripts/**/*.sh"
+rsync -a --include '*.yaml' "${GIT_ROOT}/scf/deploy/helm/scf/assets/operations"/ "${GIT_ROOT}/operations"/
 
-function pre_render_script_ops() {
-    export INSTANCE_GROUP=$1
-    export JOB=$2
-    export TYPE=$3
-    export PRE_RENDER_SCRIPT=$PRE_RENDER_SCRIPT=$4
-    export OUTPUT=$5
-    echo "Generating pre_render_script ops-file $OUTPUT"
-    scf/bosh/releases/generators/pre_render_scripts/generator.sh
-    return 0
-}
+rsync -a --include '*.yaml' "${GIT_ROOT}/scf/bazel-bin/bosh/releases"/ "${GIT_ROOT}/pre_start_scripts"/
+
+if [ "$ENABLE_EIRINI" != "true" ];then
+  rm "${GIT_ROOT}"/operations/instance_groups/bits-service.yaml
+  rm "${GIT_ROOT}"/operations/instance_groups/eirini.yaml
+  rm "${GIT_ROOT}"/operations/zz-remove-diego-if-eirini.yaml
+  rm -f "${GIT_ROOT}"/pre_start_scripts/eirini_eirini-loggregator-bridge_patch_bpm_sh.yaml
+  rm -f "${GIT_ROOT}"/pre_start_scripts/eirini_opi_patch_bpm_sh.yaml
+  rm -f "${GIT_ROOT}"/pre_start_scripts/eirini_opi_patch_opi_yml_sh.yaml
+  rm -f "${GIT_ROOT}"/pre_start_scripts/bits_bits-service_patch_bits_config_yml_sh.yaml
+fi
+
+search_instance_groups_dir="${GIT_ROOT}/operations/instance_groups/*.yaml"
+for file in $search_instance_groups_dir
+do
+  interpolate_cmd="${interpolate_cmd}  -o $file  "
+done
+
+search_pre_start_ops_dir="${GIT_ROOT}/pre_start_scripts/*.yaml"
+for file in $search_pre_start_ops_dir
+do
+  interpolate_cmd="${interpolate_cmd}  -o $file  "
+done
+
+search_common_ops_dir="${GIT_ROOT}/operations/*.yaml"
+for file in $search_common_ops_dir
+do
+  interpolate_cmd="${interpolate_cmd}  -o $file  "
+done
+
+interpolate_cmd="${interpolate_cmd}  -v releases-defaults-url=docker.io/cfcontainerization > cf-base-deployment.yaml"
+
+eval "${interpolate_cmd}"
